0ee50d1fb535764a7a2e486807867c95fc8e6aae
Prabhamani M V <prabhamani21@gmail.com>
2026-04-01 21:53:15 +0530

    Enable Swagger and Swagger UI for all environments
    Always enable Swagger and Swagger UI in the application.

--- InventoryManagementSystem/InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/InventoryManagementSystem/Program.cs
@@ -250,11 +250,8 @@ builder.Services.AddScoped<InventoryManagementSystem.Service.Interface.IJwtServi
         }
     });
     // Configure the HTTP request pipeline.
-    if (!app.Environment.IsProduction())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+ app.UseSwagger();
+app.UseSwaggerUI();
 
     if (!app.Environment.IsDevelopment())
     {
